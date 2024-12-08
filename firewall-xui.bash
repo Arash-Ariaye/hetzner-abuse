9621e08f2e6cf3a3f206e3772bb5b7b22d05b9df
Alireza Ahmand <mr.alireza.ahmand@gmail.com>
2024-12-08 11:58:03 +0330

    code refactor + more distro support.

--- firewall-xui.bash
+++ firewall-xui.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+Red="\033[1;31m"
+Green="\033[1;32m"
+Yellow="\033[1;33m"
+Blue="\033[1;34m"
+Plain="\033[0m"
+
 # Function to check firewall status and open ports
 check_ports() {
     while true; do
@@ -28,55 +34,76 @@ check_ports() {
 # Function to check and install firewall
 check_firewall() {
     clear
-    echo -e "\033[1;34mChecking firewall prerequisites...\033[0m"
+    echo -e "${Blue}Checking firewall prerequisites...${Plain}"
     
     # Check if firewall is installed
     if ! command -v ufw &> /dev/null; then
-        echo -e "\033[1;31mFirewall (ufw) is not installed. Installing...\033[0m"
-        sudo apt update && sudo apt install ufw -y
+        echo -e "${Red}Firewall (ufw) is not installed. Installing...${Plain}"
+        if [[ -f /etc/os-release ]]; then
+            source /etc/os-release
+            release=$ID
+        elif [[ -f /usr/lib/os-release ]]; then
+            source /usr/lib/os-release
+            release=$ID
+        else
+            echo "Failed to check the system OS, please contact the author!" >&2
+            exit 1
+        fi
+        echo "The OS release is: $release"
+        case "${release}" in
+        centos | fedora | almalinux)
+            yum upgrade && yum install -y -q ufw
+            ;;
+        arch | manjaro)
+            pacman -Sy --noconfirm ufw
+            ;;
+        *)
+            apt update && apt install -y -q ufw
+            ;;
+        esac
     else
-        echo -e "\033[1;32mFirewall (ufw) is installed.\033[0m"
+        echo -e "${Green}Firewall (ufw) is installed.${Plain}"
     fi
     
     # Check if firewall is active
     if ! sudo ufw status | grep -q "Status: active"; then
-        echo -e "\033[1;31mFirewall is not active. Enabling...\033[0m"
+        echo -e "${Red}Firewall is not active. Enabling...${Plain}"
         sudo ufw enable
     else
-        echo -e "\033[1;32mFirewall is active.\033[0m"
+        echo -e "${Green}Firewall is active.${Plain}"
     fi
     
-    read -p "Press Enter to return to the menu..."
+    read -r -p "Press Enter to return to the menu..."
 }
 
 # Function to start port checking in the background
 start_port_checking() {
     clear
-    echo -e "\033[1;34mPorts currently in use:\033[0m"
+    echo -e "${Blue}Ports currently in use:${Plain}"
     
     # Display ports opened by x-ui
-    echo -e "\033[1;34mPorts opened by x-ui:\033[0m"
+    echo -e "${Blue}Ports opened by x-ui:${Plain}"
     sudo lsof -i -P -n | grep x-ui | awk '{print $9}' | cut -d: -f2 | cut -d'>' -f1 | grep -Eo '^[0-9]+$'
     
     # Display ports opened by xray-linu
-    echo -e "\033[1;34mPorts opened by xray-linu:\033[0m"
+    echo -e "${Blue}Ports opened by xray-linu:${Plain}"
     sudo lsof -i -P -n | grep xray-linu | awk '{print $9}' | cut -d: -f2 | cut -d'>' -f1 | grep -Eo '^[0-9]+$'
     
     sleep 5
     
     check_ports &
     echo $! > /tmp/port_checking.pid
-    echo -ne "\033]0;3X-UI Port Checking - \033[32mActive\033[0m\007"
-    echo -e "\033[1;32mPort checking started in the background.\033[0m"
+    echo -ne "${Plain}3X-UI Port Checking - ${Green}Active${Plain}"
+    echo -e "${Green}Port checking started in the background.${Plain}"
     sleep 2
 }
 
 # Function to stop port checking
 stop_port_checking() {
     if [ -f /tmp/port_checking.pid ]; then
-        kill $(cat /tmp/port_checking.pid)
+        kill "$(cat /tmp/port_checking.pid)"
         rm /tmp/port_checking.pid
-        echo -ne "\033]0;3X-UI Port Checking - \033[31mDisabled\033[0m\007"
+        echo -ne "${Plain}3X-UI Port Checking - ${Red}Disabled${Plain}"
     fi
 }
 
@@ -84,21 +111,21 @@ stop_port_checking() {
 while true; do
     clear
     echo -e "=============================="
-    echo -e "      \033[1;34m3X-UI Port Checking\033[0m      "
+    echo -e "      ${Blue}3X-UI Port Checking${Plain}      "
     echo -e "=============================="
-    echo -e "      \033[1;33mDev by Arash\033[0m"
-    echo -e "      \033[1;33mTelegram: Arash_Ariaye\033[0m"
+    echo -e "      ${Yellow}Dev by Arash${Plain}"
+    echo -e "      ${Yellow}Telegram: Arash_Ariaye${Plain}"
     echo -e "=============================="
     if [ -f /tmp/port_checking.pid ]; then
-        echo -e "      \033[1;32mActive\033[0m"
+        echo -e "      ${Green}Active${Plain}"
     else
-        echo -e "      \033[1;31mDisabled\033[0m"
+        echo -e "      ${Red}Disabled${Plain}"
     fi
     echo -e "=============================="
-    echo -e "\033[1;32m0. Prerequisites\033[0m"
-    echo -e "\033[1;32m1. Enable Port Checking\033[0m"
-    echo -e "\033[1;31m2. Disable Port Checking\033[0m"
-    echo -e "\033[1;33m3. Exit\033[0m"
+    echo -e "${Green}0. Prerequisites${Plain}"
+    echo -e "${Green}1. Enable Port Checking${Plain}"
+    echo -e "${Red}2. Disable Port Checking${Plain}"
+    echo -e "${Yellow}3. Exit${Plain}"
     echo -e "=============================="
     read -p "Enter your choice: " choice
     
@@ -115,14 +142,14 @@ while true; do
         3)
             clear
             if [ -f /tmp/port_checking.pid ]; then
-                echo -e "\033[1;32mPort checking is currently active.\033[0m"
+                echo -e "${Green}Port checking is currently active.${Plain}"
             else
-                echo -e "\033[1;31mPort checking is currently inactive.\033[0m"
+                echo -e "${Red}Port checking is currently inactive.${Plain}"
             fi
             exit 0
             ;;
         *)
-            echo -e "\033[1;31mInvalid choice. Please try again.\033[0m"
+            echo -e "${Red}Invalid choice. Please try again.${Plain}"
             sleep 2
             ;;
     esac
